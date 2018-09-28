eaac5599aeb610a16b6aa6a87d17dda65388f378
tushar-jogi <ms14resch11003@iith.ac.in>
2018-09-28 19:58:42 +0530

    elasticity in float and project structure added

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-nvcc -Xptxas -O3 -arch=sm_60 -I/usr/local/cuda-9.1/samples/common/inc main.cu -o kks.out -lcufft -lcuda -lcudart 
+rm -f bin/kks.out
+
+nvcc -Xptxas -O3 -arch=sm_60 -I/usr/local/cuda-9.1/samples/common/inc src/main.cu -o bin/kks.out -lcufft -lcuda -lcudart 
 #nvcc -g -arch=sm_60 -I/usr/local/cuda-9.1/samples/common/inc main.cu -o kks.out -lcufft -lcuda -lcudart -lcurand -lcublas
